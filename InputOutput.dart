dcab8c4523a8e56079d5b9b3cefe6d07379c2107
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2023-09-04 18:29:17 +0600

    How to Take Input And Give Output Again

--- InputOutput.dart
+++ InputOutput.dart
@@ -5,17 +5,3 @@ void main() {
   String? name = stdin.readLineSync();
   print("My Name Is $name");
 }
-
-/*
-// importing dart:io file
-import 'dart:io';
-
-void main() {
-  print("Enter your name?");
-  // Reading name of the Geek
-  String? name = stdin.readLineSync(); // null safety in name string
-
-  // Printing the name
-  print("Hello, $name! \nWelcome to GeeksforGeeks!!");
-}
-*/
